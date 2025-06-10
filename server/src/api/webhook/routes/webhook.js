4e6d44cbfad77885490aa1caca5b0d55170a4296
saarthak10 <saarthaksharma87@gmail.com>
2025-06-10 12:06:31 +0530

    Webhook api started

---
+++ server/src/api/webhook/routes/webhook.js
@@ -0,0 +1,13 @@
+module.exports = {
+  routes: [
+    // {
+    //  method: 'GET',
+    //  path: '/webhook',
+    //  handler: 'webhook.exampleAction',
+    //  config: {
+    //    policies: [],
+    //    middlewares: [],
+    //  },
+    // },
+  ],
+};
